822e131ac45875f3a4f13b7665ca3bd1c008fd33
Menhajuddin Sharaf <menhajuddin.sharaf@gmail.com>
2021-03-18 22:17:53 +0000

    Automatically backed up by Learn

---
+++ exampleFile.rb
@@ -0,0 +1,13 @@
+puts "Hello world"
+english_music_by_city = {
+  :manchester => [
+    {
+      :band_name => "The Smiths",
+      :member_names => ["Morrissey", "Johnny", "Andy", "Mike"]
+    },
+    {
+      :band_name => "Joy Division",
+      :member_names => ["Peter", "Stephen", "Bernard", "Ian"]
+    },
+  ]
+}
